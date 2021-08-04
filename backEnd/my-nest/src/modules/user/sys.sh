609097e568dbecc7e5840b8a7649bf7d5cccaff6
qjh <qjh886@qq.com>
2021-08-04 17:22:45 +0800

    -fix

--- backEnd/my-nest/src/modules/user/sys.sh
+++ backEnd/my-nest/src/modules/user/sys.sh
@@ -2,8 +2,8 @@
 cd /www/res/my-blog && 
 git pull &&
 cd /www/res/my-blog/backEnd/my-nest && 
+cp -r /www/res/my-blog/backEnd/my-nest/* /www/back-end/nest/ &&
 cnpm install && 
-cp -r /www/res/my-blog/backEnd/my-nest/* /www/back-end/nest/
 npm run build && 
 pm2 restart 2 
 echo "后台编译成功"
