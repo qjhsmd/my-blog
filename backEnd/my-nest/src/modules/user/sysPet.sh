4883164ee83f80eb2825d2f65a621e934f41b7e6
qjh <qjh886@qq.com>
2021-11-18 11:25:07 +0800

    ..

--- backEnd/my-nest/src/modules/user/sysPet.sh
+++ backEnd/my-nest/src/modules/user/sysPet.sh
@@ -4,12 +4,12 @@
  # @Author: QJH
  # @Date: 2021-11-18 10:49:18
  # @LastEditors: QJH
- # @LastEditTime: 2021-11-18 10:52:32
+ # @LastEditTime: 2021-11-18 11:24:07
 ### 
 cd /www/res/pet && 
 git pull &&
 cnpm install &&
 npm run build &&
 rm -rf /www/front-end/pet/* &&
-cp -r /www/res/pet/dist/* /www/front-end/blog/
+cp -r /www/res/pet/dist/* /www/front-end/pet/
 echo "迪大爷病例管理系统编译成功"
