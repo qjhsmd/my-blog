fdb7a674a7377a3d58c55eee5eda42cb83a43988
qjh <qjh886@qq.com>
2022-02-21 16:46:18 +0800

    all

--- backEnd/my-nest/src/modules/user/sys.sh
+++ backEnd/my-nest/src/modules/user/sys.sh
@@ -2,8 +2,8 @@
 ###
  # @Author: your name
  # @Date: 2021-07-29 15:27:04
- # @LastEditTime: 2022-02-21 16:18:29
- # @LastEditors: your name
+ # @LastEditTime: 2022-02-21 16:44:34
+ # @LastEditors: Please set LastEditors
  # @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  # @FilePath: \my-nest\src\modules\user\sys.sh
 ### 
@@ -13,5 +13,5 @@ cp -r /www/res/my-blog/backEnd/my-nest/* /www/back-end/nest/ &&
 cd /www/back-end/nest && 
 npm install && 
 npm run build && 
-pm2 restart 2 
+pm2 restart 0
 echo "后台编译成功"
