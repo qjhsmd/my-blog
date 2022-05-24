1fd4e7d34abcc8fc8b8c0bc86b3bff5a32b504ec
qjh <qjh886@qq.com>
2022-05-24 17:17:30 +0800

    。。

--- sh/sys.sh
+++ sh/sys.sh
@@ -2,14 +2,15 @@
 ###
  # @Author: your name
  # @Date: 2021-07-29 15:27:04
- # @LastEditTime: 2022-05-24 17:09:12
+ # @LastEditTime: 2022-05-24 17:10:28
  # @LastEditors: QJH
  # @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  # @FilePath: \my-nest\src\modules\user\sys.sh
 ### 
 cd /www/res/my-blog && 
 git pull &&
-cp -r /www/res/my-blog/backEnd/my-nest/* /www/back-end/nest/ &&
+rm -rf /www/back-end/nest/* &&
+cp -r /www/res/my-blog/backEnd/my-nest/ /www/back-end/nest/ &&
 cd /www/back-end/nest && 
 cnpm install && 
 npm run build && 
