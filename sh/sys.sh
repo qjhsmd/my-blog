17f5007d39c8a67903b6292d92d18c6fdb290430
qjh <qjh886@qq.com>
2022-05-25 10:07:51 +0800

    测试

--- sh/sys.sh
+++ sh/sys.sh
@@ -2,12 +2,12 @@
 ###
  # @Author: your name
  # @Date: 2021-07-29 15:27:04
- # @LastEditTime: 2022-05-25 10:00:43
+ # @LastEditTime: 2022-05-25 10:07:39
  # @LastEditors: QJH
  # @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  # @FilePath: \my-nest\src\modules\user\sys.sh
 ### 
-echo "开始执行脚本"
+echo "开始执行脚本。。。"
 cd /www/res/my-blog && 
 git pull &&
 rm -rf /www/back-end/nest/* &&
@@ -15,5 +15,5 @@ cp -r /www/res/my-blog/backEnd/my-nest/* /www/back-end/nest/ &&
 cd /www/back-end/nest && 
 cnpm install && 
 npm run build &&
-echo "后台编译成功，正在重启"
+echo "后台编译成功，正在重启。。。"
 pm2 restart 0
